5ebc99cbb986db18067f5a34be23d4cf5437f180
Sambo Chea <sombochea@cubetiqs.com>
2020-06-09 09:21:51 +0700

    Add sample module gradle

---
+++ src/main/java/Print.java
@@ -0,0 +1,13 @@
+/**
+ * Print Message
+ *
+ * @author sombochea
+ * @since 1.0
+ */
+public class Print {
+    public static final String ID = "PRINT";
+
+    public static void print(String message) {
+        System.out.println(ID + " : " + message);
+    }
+}
